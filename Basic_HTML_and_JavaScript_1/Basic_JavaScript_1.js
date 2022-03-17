c1c077881fcb75dfe09a24d5b333d77dd3fc2caa
Peter Falk <falkman13@gmail.com>
2022-03-17 09:33:47 -0400

    New function test file

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,1 +1,7 @@
-alert("Hello, world! I am an alert box!");
+alert("This is a button based test. No cubes will be involved.");
+function My_First_Function() {
+	var String = "Kiss me, I'm Irish";
+	var result = String.fontcolor("green");
+	document.getElementById("Irish").innerHTML =
+	result;
+}
